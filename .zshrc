192da15cfc2e739bdc77bc076838b5644efac2c6
LotuSrc <451415738@qq.com>
2022-01-03 14:47:09 +0800

    fix: install.sh

--- .zshrc
+++ .zshrc
@@ -13,7 +13,7 @@ fi
 export PATH=$HOME/.local/bin:$PATH
 
 # Path to your oh-my-zsh installation.
-export ZSH="/Users/lotus/.oh-my-zsh"
+export ZSH="$HOME/.oh-my-zsh"
 
 # Set name of the theme to load --- if set to "random", it will
 # load a random theme each time oh-my-zsh is loaded, in which case,
@@ -121,19 +121,3 @@ source $ZSH/oh-my-zsh.sh
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/Users/lotus/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/Users/lotus/miniconda3/etc/profile.d/conda.sh" ]; then
-        . "/Users/lotus/miniconda3/etc/profile.d/conda.sh"
-    else
-        export PATH="/Users/lotus/miniconda3/bin:$PATH"
-    fi
-fi
-unset __conda_setup
-# <<< conda initialize <<<
-
--- install.sh
+++ install.sh
@@ -6,85 +6,118 @@ MIRROR=N
 PROXY=N
 PROXY_URL=https://ghproxy.com
 
-# 安装zsh和neovim
-if [[ $OS = 'macos' ]]
-then
-  echo "[00] install homebrew"
-  if [[ $MIRROR = 'Y' ]]
-  then
-    export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
-    export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tunz.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
-    export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
-    sh -c "$(curl -fsSL https://github.com/Homebrew/install/raw/master/install.sh)"
-    if [[ $(uname -m) = 'arm64' ]]
-    then
-      test -r ~/.bash_profile && echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.bash_profile
-      test -r ~/.zprofile && echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
-    fi
-  echo "[01] install [zsh|neovim]"
-  brew install zsh
-  brew install neovim
-  fi
-else
-  sudo echo -e "\c"
-  if [[ $? = 1 ]]
-  then
-    echo "[00] plz use root privilege to install zsh!"
-    exit 1
-  fi
-  echo "[01] install [zsh|neovim]"
-  if [[ $OS = 'centos' ]]
-  then
-    sudo dnf install -y zsh
-    sudo dnf install -y ninja-build libtool autoconf automake cmake gcc gcc-c++ make pkgconfig unzip patch gettext curl
-    if [[ $PROXY = 'Y' ]]
-    then
-      git clone -b stable --depth=1 --quiet $PROXY_URL/https://github.com/neovim/neovim
-    else
-      git clone -b stable --depth=1 --quiet https://github.com/neovim/neovim
-    fi
-    make CMAKE_BUILD_TYPE=Release -C neovim 2>&1 >/dev/null
-    sudo make install -C neovim 2>&1 >/dev/null
-    rm -rf neovim
-  else
-    sudo apt-get install -y -qq zsh
-    sudo apt-get install -y -qq ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
-    if [[ $PROXY = 'Y' ]]
-    then
-      git clone -b stable --depth=1 --quiet $PROXY_URL/https://github.com/neovim/neovim 
-    else
-      git clone -b stable --depth=1 --quiet https://github.com/neovim/neovim
-    fi
-    make CMAKE_BUILD_TYPE=Release -C neovim 2>&1 >/dev/null
-#     cd neovim && git checkout stable && make
-    sudo make install -C neovim 2>&1 >/dev/null
-    rm -rf neovim
-  fi
-fi
+OS=`uname -a`
 
-if [[ $PROXY = 'Y' ]]
+COLOR_GRAY="\033[1;38;5;243m"
+COLOR_BLUE="\033[1;34m"
+COLOR_GREEN="\033[1;32m"
+COLOR_RED="\033[1;31m"
+COLOR_PURPLE="\033[1;35m"
+COLOR_YELLOW="\033[1;33m"
+COLOR_NONE="\033[0m"
+
+function title() {
+  echo -e "\n${COLOR_PURPLE}$1${COLOR_NONE}"
+  echo -e "${COLOR_GRAY}==============================${COLOR_NONE}\n"
+}
+
+function error() {
+  echo -e "${COLOR_RED}Error: ${COLOR_NONE}$1"
+  exit 1
+}
+
+function warning() {
+  echo -e "${COLOR_YELLOW}Warning: ${COLOR_NONE}$1"
+}
+
+function info() {
+  echo -e "${COLOR_BLUE}Infor: ${COLOR_NONE}$1"
+}
+
+function success() {
+  echo -e "${COLOR_GREEN}$1${COLOR_NONE}"
+}
+
+function setup_homebrew() {
+  title "Setting up Homebrew"
+
+  if test ! "$(command -v brew)"; then
+    info "Homebrew not installed. Installing."
+    export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
+    export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
+    export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
+    git clone --quiet --depth=1 https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/install.git brew-install
+    /bin/bash brew-install/install.sh
+    rm -rf brew-install
+  fi
+ 
+  if 
+}
+
+
+if [[ $OS =~ 'Darwin' ]]
 then
-  echo "[02] proxy switch on: $PROXY_URL"
-  echo "[03] plugin install [ohmyzsh|zsh-autosuggestions|zsh-syntax-highlighting]"
-  sh -c "$(curl -fsSL $PROXY_URL/https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-  git clone --quiet $PROXY_URL/https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-  git clone --quiet $PROXY_URL/https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-  echo "[04] install lunarvim"
-  sh -c "$(curl -s $PROXY_URL/https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)"
-  echo "[05] install powerlevel10k theme"
-  git clone --depth=1 $PROXY_URL/https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-  
+  echo "install homebrew"
+  export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
+  export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
+  export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
+  git clone --depth=1 https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/install.git brew-install
+  /bin/bash brew-install/install.sh
+  rm -rf brew-install
+  test -r ~/.bash_profile && echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.bash_profile
+  test -r ~/.zprofile && echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+
+  test -r ~/.zprofile && echo 'export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"' >> ~/.zprofile
+
+elif [[ $OS =~ 'Linux' ]]
+then
+  echo "install linuxbrew"
+  export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
+  export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
+  export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
+  git clone --depth=1 https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/install.git brew-install
+  /bin/bash brew-install/install.sh
+  rm -rf brew-install
+  test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+  test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+  test -r ~/.bash_profile && echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bash_profile
+  test -r ~/.profile && echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.profile
+  test -r ~/.zprofile && echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.zprofile
+
+  test -r ~/.zprofile && echo 'export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"' >> ~/.zprofile
+
 else
-  echo "[02] proxy switch off"
-  echo "[03] plugin install [ohmyzsh|zsh-autosuggestions|zsh-syntax-highlighting]"
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-  git clone --quiet https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-  git clone --quiet https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-  echo "[04] install lunarvim"
-  sh -c "$(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)"
-  echo "[05] install powerlevel10k theme"
-  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-fi
+  echo "unrecognized os: $OS"
+  exit 1
+
+
+
+
+
+echo "install zsh"
+brew install -q zsh
+echo "install ohmyzsh"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+echo "plugin install zsh-autosuggestions"
+git clone --quiet https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+echo "plugin install zsh-syntax-highlighting"
+git clone --quiet https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+echo "plugin install autojump"
+brew install -q autojump
+echo "theme install powerlevel10k"
+git clone --quiet --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+
+
+
+echo "install neovim"
+brew install -q neovim
+echo "install lunarvim dependencies [npm|node|cargo]"
+brew install -q node
+brew install -q rust
+echo "install lunarvim"
+/bin/bash -c "$(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)"
+
 
 echo "[06] cp conf [.zshrc|.p10k.zsh|config.lua]"
 cp -n ~/dotfiles/conf/.zshrc ~
